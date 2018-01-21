bd6472d28f92fefe8c94e47e2e95a358a7b2e3d1
drhansen000 <drhansen000@hotmail.com>
2018-01-20 17:42:10 -0700

    Edited team activity02 to work with new directory layout

--- web/team/activity02/nav.php
+++ web/team/activity02/nav.php
@@ -1,28 +1,19 @@
-<link rel="stylesheet" type="text/css" href="/bootstrap-css/bootstrap.css">
-<nav class="navbar navbar-toggleable-md navbar-light bg-faded">
-  <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <a class="navbar-brand" href="#">Navbar</a>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item <?php checkFile("home.php") ?>">
-        <a class="nav-link" href="home.php">Home</a>
-      </li>
-      <li class="nav-item <?php checkFile("login.php") ?>">
-        <a class="nav-link" href="login.php">Login</a>
-      </li>
-      <li class="nav-item <?php checkFile("about-us.php") ?>">
-        <a class="nav-link" href="about-us.php">About Us</a>
-      </li>
+<link rel="stylesheet" type="text/css" href="../bootstrap-css/bootstrap.css">
+<nav class="navbar bg-dark navbar-expand-sm navbar-dark">
+    <ul class="navbar-nav">
+        <li class="nav-item <?php checkFile("home.php") ?>">
+            <a class="nav-link" href="home.php">Home</a>
+        </li>
+        <li class="nav-item <?php checkFile("login.php") ?>">
+            <a class="nav-link" href="login.php">Login</a>
+        </li>
+        <li class="nav-item <?php checkFile("about-us.php") ?>">
+            <a class="nav-link" href="about-us.php">About Us</a>
+        </li>
         <li class="nav-item">
-        <a class="nav-link" href="logout.php">Logout</a>
-      </li>
+            <a class="nav-link" href="logout.php">Logout</a>
+        </li>
     </ul>
-    <form class="form-inline my-2 my-lg-0">
-    </form>
-  </div>
 </nav>
 
 <?php
