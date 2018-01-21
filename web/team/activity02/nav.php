6bee56fcc0124fda602b45ac18adbc6f892cfe16
drhansen000 <drhansen000@hotmail.com>
2018-01-20 22:44:11 -0700

    Changed code to work on heroku, not just local server

--- web/team/activity02/nav.php
+++ web/team/activity02/nav.php
@@ -1,13 +1,19 @@
+<?php
+        //This code was taken from Bro. Burton's team solution
+        // From StackOverflow: http://stackoverflow.com/a/12201089/1932766
+        // The second argument to pathinfo() strips the path and extension from the file name (PHP >= 5.2)
+        $file = pathinfo($_SERVER['PHP_SELF'], PATHINFO_FILENAME);
+?>
 <link rel="stylesheet" type="text/css" href="../bootstrap-css/bootstrap.css">
 <nav class="navbar bg-dark navbar-expand-sm navbar-dark">
     <ul class="navbar-nav">
-        <li class="nav-item <?php checkFile("home.php") ?>">
+        <li class="nav-item <?php if ($file === 'home') echo 'active' ?>">
             <a class="nav-link" href="home.php">Home</a>
         </li>
-        <li class="nav-item <?php checkFile("login.php") ?>">
+        <li class="nav-item <?php if ($file === 'login') echo 'active' ?>">
             <a class="nav-link" href="login.php">Login</a>
         </li>
-        <li class="nav-item <?php checkFile("about-us.php") ?>">
+        <li class="nav-item <?php if ($file === 'about-us') echo 'active' ?>">
             <a class="nav-link" href="about-us.php">About Us</a>
         </li>
         <li class="nav-item">
@@ -15,13 +21,3 @@
         </li>
     </ul>
 </nav>
-
-<?php
-    function checkFile($fileName)
-    {
-        if ($_SERVER["SCRIPT_FILENAME"] == "/home/drhansen000/lappstack-5.6.32-1/apache2/htdocs/cs313-php/web/team/activity02/" . $fileName) 
-        {
-            echo("active");
-        }
-    }
-?>
