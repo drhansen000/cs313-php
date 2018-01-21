6c0ab14c92df2512f9d9a5b201a567fef284a476
drhansen000 <drhansen000@hotmail.com>
2018-01-20 22:51:12 -0700

    Changed code for HH&N

--- web/hh&n/navbar.php
+++ web/hh&n/navbar.php
@@ -1,19 +1,26 @@
+<?php
+        //This code was taken from Bro. Burton's team solution
+        // From StackOverflow: http://stackoverflow.com/a/12201089/1932766
+        // The second argument to pathinfo() strips the path and extension from the file name (PHP >= 5.2)
+        $file = pathinfo($_SERVER['PHP_SELF'], PATHINFO_FILENAME);
+?>
+
 <!DOCTYPE html/>
 <html>
 <head></head>
 <body>
     <nav class="navbar navbar-expand-sm navbar-dark fixed-top">
         <ul class="navbar-nav">
-            <li class="nav-item <?php checkFile("offer.php") ?>" >
+            <li class="nav-item <?php if ($file === 'offer') echo 'active' ?>" >
                 <a class="nav-link" href="offer.php">What We Offer</a>
             </li>
-            <li class="nav-item <?php checkFile("products.php") ?>" >
-                <a class="nav-link" href="products.php">Previous Appointments</a>
+            <li class="nav-item <?php if ($file === 'products') echo 'active' ?>" >
+                <a class="nav-link" href="products.php">Product Store</a>
             </li>
-            <li class="nav-item <?php checkFile("previous.php") ?>" >
-                    <a class="nav-link" href="previous.php">Future Appointments</a>                
+            <li class="nav-item <?php if ($file === 'previous') echo 'active' ?>" >
+                    <a class="nav-link" href="previous.php">Previous Appointments</a>                
             </li>
-            <li class="nav-item <?php checkFile("future.php") ?>" >
+            <li class="nav-item <?php if ($file === 'future') echo 'active' ?>" >
                     <a class="nav-link" href="future.php">Future Appointments</a>                
             </li>
         </ul>
