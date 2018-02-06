ed43d9eeba6a20243f118c7f7dc28fd361bcef99
drhansen000 <drhansen000@hotmail.com>
2018-02-06 00:53:59 -0700

    trying to connect to psql through heroku

--- web/hh&n/addToCart.php
+++ web/hh&n/addToCart.php
@@ -2,17 +2,9 @@
     session_start();
 
     //connect to database and insert information
-// default Heroku Postgres configuration URL
 $dbUrl = getenv('DATABASE_URL');
 
-if (empty($dbUrl)) {
- // example localhost configuration URL with postgres username and a database called cs313db
- $dbUrl = "postgres://postgres:password@localhost:5432/hhn";
-}
-
 $dbopts = parse_url($dbUrl);
-
-print "<p>$dbUrl</p>\n\n";
 
 $dbHost = $dbopts["host"];
 $dbPort = $dbopts["port"];
@@ -20,15 +12,7 @@ $dbUser = $dbopts["user"];
 $dbPassword = $dbopts["pass"];
 $dbName = ltrim($dbopts["path"],'/');
 
-print "<p>pgsql:host=$dbHost;port=$dbPort;dbname=$dbName</p>\n\n";
-
-try {
- $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-}
-catch (PDOException $ex) {
- print "<p>error: $ex->getMessage() </p>\n\n";
- die();
-}
+$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 foreach ($db->query('SELECT stuff FROM filler') as $row)
 {
   echo 'stuff: ' . $row['stuff'];
