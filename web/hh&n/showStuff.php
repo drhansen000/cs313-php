d6100df9885e5126ebbdc66f9c00a973210f0a22
drhansen000 <drhansen000@hotmail.com>
2018-02-28 12:35:10 -0700

    encode the information into JSON

--- web/hh&n/showStuff.php
+++ web/hh&n/showStuff.php
@@ -1,16 +1,35 @@
 <?php
 include("connect.php");
 
+    class Service
+    {
+        var $id;
+        var $service;
+        var $cost;
+        var $duration;
+//        
+        function __construct($id, $service, $cost, $duration)
+        {
+            $this->id = $id;
+            $this->service = $service;
+            $this->cost = $cost;
+            $this->duration = $duration;
+        }
+    }
+
+    $services = array();
+
+
     $statement = $db->prepare("SELECT id, service, cost, duration FROM service");
     $statement->execute();
-    $services = $statement->fetchAll(PDO::FETCH_ASSOC);
+    $getData = $statement->fetchAll(PDO::FETCH_ASSOC);
 
-    foreach ($services as $service)
+    $i = 0;
+    foreach ($getData as $service)
     {
-        echo("Id: " . $service['id'] . "<br/>");
-        echo("Service: " . $service['service'] . "<br/>");
-        echo("Cost: " . $service['cost'] . "<br/>");
-        echo("Duration: " . $service['duration'] . "<br/>");
+        $services[$i] = new Service($service['id'], $service['service'], $service['cost'], $service['duration']);
+        $i++;
     }
+    echo(json_encode($services));
 
 ?>
